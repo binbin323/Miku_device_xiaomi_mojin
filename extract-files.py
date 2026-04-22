a2b96932a70dcdc71e2bf638d63a06eca726816e
binbin323 <bin@dhao.cc>
2026-04-22 12:01:32 +0800

    mojin: Adopt Python extract utils

---
+++ extract-files.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2022 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/mojin',
+    'hardware/qcom-caf/sm8350',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/perf',
+]
+
+
+def lib_fixup_vendor_variants(lib: str, partition: str, *args, **kwargs):
+    if partition != 'vendor':
+        return None
+
+    if lib in {
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    }:
+        return f'{lib}-vendor'
+
+    return None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_variants,
+    ('libOmxCore', 'libwpa_client'): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/bin/wfdservice64': blob_fixup()
+        .add_needed('libwfdservice_shim.so'),
+    'system_ext/etc/init/wfdservice.rc': blob_fixup()
+        .regex_replace(r'start wfdservice', 'start wfdservice64')
+        .regex_replace(r'stop wfdservice', 'stop wfdservice64'),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudioclient_shim.so')
+        .replace_needed(
+            'android.media.audio.common.types-V2-cpp.so',
+            'android.media.audio.common.types-V4-cpp.so',
+        )
+        .replace_needed(
+            'android.media.audio.common.types-V3-cpp.so',
+            'android.media.audio.common.types-V4-cpp.so',
+        ),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libbinder_shim.so')
+        .add_needed('libinput_shim.so'),
+    (
+        'vendor/etc/media_lahaina/video_system_specs.json',
+        'vendor/etc/media_shima_v1/video_system_specs.json',
+        'vendor/etc/media_yupik_v1/video_system_specs.json',
+    ): blob_fixup()
+        .regex_replace(
+            r'("max_retry_alloc_output_timeout"\s*:\s*)10000',
+            r'\g<1>0',
+        ),
+    'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
+        .regex_replace(r'^.*(?:ozoaudio|dolby).*(?:\n|$)', ''),
+    ('vendor/lib/hw/audio.primary.lahaina.so', 'vendor/lib/libaudioroute_ext.so'): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'mojin',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
