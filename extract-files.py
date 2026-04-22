0c4d1a1e182dfa487928768cd14e0fb49ed36baa
MikeNG <mikeNG@users.noreply.github.com>
2026-04-22 14:43:39 +0800

    mojin: Patch some blobs to depend on libtinyxml2-v34.so libtinyxml2 10.1.0 update which landed in BP4A contains ABI incompatible changes which cause runtime crashes.

--- extract-files.py
+++ extract-files.py
@@ -97,6 +97,12 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    (
+        'vendor/lib64/libdpps.so',
+        'vendor/lib64/libsnapdragoncolor-manager.so',
+        'vendor/lib64/libvideooptfeature.so'
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
